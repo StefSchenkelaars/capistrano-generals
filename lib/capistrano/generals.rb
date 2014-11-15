0a9557b38784bceaede1d03ac431e861b3fb4eb6
Stef Schenkelaars <stef.schenkelaars@gmail.com>
2014-11-15 16:42:06 +0100

    Initial commit

---
+++ lib/capistrano/generals.rb
@@ -0,0 +1,7 @@
+require "capistrano/generals/version"
+
+module Capistrano
+  module Generals
+    # Your code goes here...
+  end
+end
---
+++ lib/capistrano/generals/version.rb
@@ -0,0 +1,5 @@
+module Capistrano
+  module Generals
+    VERSION = "0.0.1"
+  end
+end
